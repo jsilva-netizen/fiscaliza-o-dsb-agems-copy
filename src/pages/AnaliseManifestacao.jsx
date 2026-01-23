c54c7cd3a413ab7f74bc71168bbf0fdfe799fd18
João Lucas Silva <collectivenahual@gmail.com>
2026-01-23 16:25:22 +0000

    File changes

--- src/pages/AnaliseManifestacao.jsx
+++ src/pages/AnaliseManifestacao.jsx
@@ -604,6 +604,7 @@ export default function AnaliseManifestacao() {
                             </Button>
                         )}
                         </div>
+                        </div>
                         </CardContent>
                         </Card>
 
