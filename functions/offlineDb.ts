f1878bacad0bcbc0dd3c9efaeea861db09ea8cb2
jsilva-netizen <jsilva@agems.ms.gov.br>
2026-02-10 09:32:26 -0400

    Delete functions/offlineDb.ts

--- functions/offlineDb.ts
+++
@@ -1,6 +0,0 @@
-
-// Este arquivo foi substituído por offlineDbFallback.js
-// que implementa suporte a IndexedDB com fallback para localStorage
-
-export { default } from './offlineDbFallback';
-export { default as db } from './offlineDbFallback';
